509665250db039a66aa565a8c1270715457f692f
“DanielLazo” <john.lazo@epn.edu.ec>
2023-02-11 01:44:48 -0500

    Se modifico los segundos de las advertencias Daniel Lazo

--- Proyecto/js/Transferencias.js
+++ Proyecto/js/Transferencias.js
@@ -22,7 +22,7 @@ function transferencia(cuenta) {
 
 //Evento que cuando deje de escribir verifica que cumpla las condiciones
 $Fmonto.addEventListener('keydown', () => {
-    //cada segundo verifica si a dejado de escribir
+    //cada 3 segundos se verifica si a dejado de escribir
     timeout = setTimeout(() => {
         monto = document.getElementById("cuadroMonto").value;
         const MAX_MONTO = 5000;
@@ -30,7 +30,7 @@ $Fmonto.addEventListener('keydown', () => {
             toastr.options={
                 "positionClass": "toast-top-right",
                 "preventDuplicates": true,
-                "timeOut": "4000",
+                "timeOut": "1500",
             }
             toastr.success("Monto correcto");
             document.getElementById("cuadroNumCuenta").disabled = false;
@@ -39,7 +39,7 @@ $Fmonto.addEventListener('keydown', () => {
             toastr.options={
                 "positionClass": "toast-top-center",
                 "preventDuplicates": true,
-                "timeOut": "4000",
+                "timeOut": "1500",
             }
             if (monto >= MAX_MONTO && monto <= saldo && monto!=0)
                 toastr.error("El monto máximo por transacción diaria es de 5000 dólares");
@@ -49,7 +49,7 @@ $Fmonto.addEventListener('keydown', () => {
                 toastr.error("No se ha ingresado ningun valor");
             document.getElementById("cuadroNumCuenta").disabled = true;              
         }
-    }, 1000);    
+    }, 3000);    
 })
 
 //Evento que cuando deje de escribir verifica que cumpla las condiciones
@@ -64,7 +64,7 @@ $FnumeroC.addEventListener('keydown', () => {
             toastr.options={
                 "positionClass": "toast-top-center",
                 "preventDuplicates": true,
-                "timeOut": "4000",
+                "timeOut": "1500",
             }
             toastr.error("El numero de cuenta debe estar entre 8 a 10 numeros");
             document.getElementById("btnVerificar").disabled = true;
