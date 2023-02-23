92d0e9c9d54b3a6ee9d8713b42b54570f44e4c71
“DanielLazo” <john.lazo@epn.edu.ec>
2023-02-22 21:30:37 -0500

    feat: Agregadas las rutas de los nuevos modulos Daniel Lazo

--- Proyecto/Banco/src/app/app-routing.module.ts
+++ Proyecto/Banco/src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
 import { CredencialesComponent } from './credenciales/credenciales.component';
 import { CuentaComponent } from './cuenta/cuenta.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { MiCuentaComponent } from './mi-cuenta/mi-cuenta.component';
+import { MiPerfilComponent } from './mi-perfil/mi-perfil.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { PagoTarjetasComponent } from './pago-tarjetas/pago-tarjetas.component';
 import { TransferenciaComponent } from './transferencia/transferencia.component';
 
 const routes: Routes = [
   {path:'inicio',component:InicioComponent},
+  {path:'contacto',component:ContactoComponent},
+  {path:'nosotros',component:NosotrosComponent},
   {path:'cuenta',component:CuentaComponent},
   {path:'credenciales',component:CredencialesComponent},
   {path:'transferencias',component:TransferenciaComponent},
+  {path:'pago-tarjetas',component:PagoTarjetasComponent},
+  {path:'mi-cuenta',component:MiCuentaComponent},
+  {path:'mi-perfil',component:MiPerfilComponent},
   {path:'**',component:InicioComponent},
 ];
 
